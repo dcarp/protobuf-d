a94954de3314fdcceb68de7b1f02ef7fae42b01d
Dragos Carp <dragoscarp@gmail.com>
2018-05-29 23:19:05 +0200

    Add conformance tests

---
+++ conformance/run-conformance-test.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+if cd protobuf >/dev/null 2>&1; then
+	git fetch
+else
+	git clone https://github.com/google/protobuf.git && cd protobuf
+fi
+git checkout tags/v3.5.2 --detach
+
+git submodule update --init --recursive
+./autogen.sh && ./configure && make
+
+cd conformance
+make
+
+cd ../..
+
+./protobuf/src/protoc --plugin=../build/protoc-gen-d --d_out=. -I. -Iprotobuf/src protobuf/conformance/conformance.proto protobuf/src/google/protobuf/test_messages_proto3.proto
+./protobuf/conformance/conformance-test-runner --failure_list failure_list_d.txt ./conformance-d
